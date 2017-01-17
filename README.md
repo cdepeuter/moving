ee695533453b8c83dfaae4dbc5eaaea7df77ea43
Conrad De Peuter <conrad.depeuter@gmail.com>
2017-01-17 15:08:30 -0500

    walkthrough notebook + cleanup + readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Using python to find an apartment in New York (or anywhere you want!)
+
+This repo contains a python module with which I ran every minute (Ubuntu Crontask) to help get real time notifications of new apartment listings. A walkthrough of the code is in streetEasy.ipynb and the actual python module is in find_apy.py. It won't work out of the box as you'll have to update the email accounts/passwords in sendMail().
--- find_apt.py
+++ find_apt.py
@@ -9,42 +9,56 @@ import smtplib
 import datetime
 
 def getPlace(pl):
-
-    apt = {'price':pl.select("span.price")[0].text, 'loc':pl["se:map:point"], "id":pl["data-id"]}
-    apt["addr"] = pl.select("div.details-title a")[0].text
-    apt["broker_fee"] = len(pl.select("div.banner.no_fee")) > 0
-    apt["dist"] = getDistFromC(apt['loc'])
-
-    apt_size = pl.select("span.last_detail_cell")[0] if len(pl.select("span.last_detail_cell")) > 0 else None
+    # scrape relevant info for an apartment, return as dict
+    apt = {'price':pl.select_one("span.price").text, 'loc':pl["se:map:point"], "id":pl["data-id"]}
+    apt["addr"] = pl.select_one("div.details-title a").text
+    
+    # does it have a brokers fee?
+    apt["broker_fee"] = pl.select_one("div.banner.no_fee") is None
+    
+    # how far from Columbia?
+    apt["dist"] = getDistFromColumbia(apt['loc'])
+    
+    # sometimes the size is not available
+    apt_size = pl.select_one("span.last_detail_cell")
     if apt_size is not None:
         apt["size"] = apt_size.text
-    
+        
+    # what neighborhood?
     apt["nbh"] = pl(text=re.compile(r' in '))[0]
-    apt["href"] = "http://streeteasy.com"+pl.select("div.details-title a")[0]["href"]
+    
+    # url
+    apt["href"] = "http://streeteasy.com" + pl.select_one("div.details-title a")["href"]
     
     return apt
 
-def getDistFromC(coords):
+def getDistFromColumbia(coords):
+    #given a set of coordinates, how far in miles is that location from Columbia
     splt = coords.split(",")
-    clmb = (40.807511,-73.9647077) #columbia 
-    dist = vincenty(clmb, splt).miles
+    columbia = (40.807511,-73.9647077)
+    dist = vincenty(columbia, splt).miles
     return dist
 
+
+
 def sendMail(apt):
-    fromMy = 'cdpconrad2119@yahoo.com' 
-    to  = 'conrad.depeuter@gmail.com'
+    # send an email with the apartment information
+    fromMy = ''
+    to  = ''
     subj='New Apt: ' + apt["price"] + ", Dist: "+str(apt["dist"])
     message_text=str(apt).encode('ascii', 'ignore')
     msg = "From: %s\nTo: %s\nSubject: %s\n\n%s" % ( fromMy, to, subj, message_text )
 
-    username = str() #your email  
-    password = str() #your pass  
+    username = ''  
+    password = ''
     server = smtplib.SMTP("smtp.mail.yahoo.com",587)
     server.ehlo()
     server.starttls()
     server.login(username,password)
     server.sendmail(fromMy, to,msg)
     server.quit()
+    
+
 
 def main():
     steasy = "http://streeteasy.com/for-rent/nyc/price:1000-2000%7Carea:154,148,153,147%7Cbeds%3C1?sort_by=listed_desc"
@@ -53,13 +67,14 @@ def main():
     stplaces = soup.select("div.item")
 
     places = [getPlace(s) for s in stplaces]
+    
     #places already sent
     with open('places.txt', 'r+') as f:
         placeIds = f.read()
         for p in places:
             pSplit = placeIds.split(",")
             if p["id"] not in pSplit:
-                print("FOUND APT", p["id"])
+                #print("FOUND APT", p["id"])
                 sendMail(p)
                 f.write(p["id"]+",")
         f.close()
@@ -69,8 +84,6 @@ def main():
     with open("time.txt", 'w+') as wr:
         fmt='%Y-%m-%d-%H-%M-%S'
         wr.write(datetime.datetime.now().strftime(fmt));
-
-    print("DONE")
 
 
 if __name__ == "__main__":
